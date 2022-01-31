68c896b320a4efad2da155243a927d79d86b9391
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-31 14:22:46 +0100

    Initial commit

---
+++ src/main/java/at/ht/control/VehicleResource.java
@@ -0,0 +1,39 @@
+package at.ht.control;
+
+import at.htl.entity.Vehicle;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/vehicle")
+public class VehicleResource {
+
+    private final List<Vehicle> vehicles = List.of(
+            new Vehicle("Opel", "Kadett"),
+            new Vehicle("Opel", "Astra"),
+            new Vehicle("VW", "Golf"),
+            new Vehicle("VW", "Passat"),
+            new Vehicle("VW", "Tiguan"),
+            new Vehicle("VW", "Touareg"),
+            new Vehicle("VW", "Tupolev"),
+            new Vehicle("VW", "Golf Plus"),
+            new Vehicle("VW", "Golf R"),
+            new Vehicle("VW", "Golf GTI")
+    );
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance vehicle(List<Vehicle> vehicles);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance getVehicles() {
+        return Templates.vehicle(vehicles);
+    }
+}
